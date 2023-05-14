1a4041fb522ea00e52ec15bc523684df3cdc484f
Karabo Kgatla <kkgatla09@gmail.com>
2023-05-14 19:37:27 +0200

    Send data from frontend to backend

--- SRC/Public/scripts/appointments.js
+++ SRC/Public/scripts/appointments.js
@@ -23,7 +23,34 @@ document.addEventListener('DOMContentLoaded', function () {
       const eventTitleError = document.getElementById('eventTitleError')
 
       validateEventTitle(eventTitle, eventTitleError)
+      
+      const data = {
+        eventTitle: eventTitle
+      }
+      postJSON(data)
     })
   }
 })
-  
+
+
+function postJSON(data) {
+  const baseURL = 'http://localhost:3000'
+  fetch(baseURL + '/scheduleAppointment', {
+    method: 'post',//specify method to use
+    headers: {//headers to specify the type of data needed
+    'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  })
+  .then(function(response) {
+    if(response.ok)
+      return response.json(); // Return the response parse as JSON if code is valid
+    else
+      throw 'Failed!'
+  }).catch(function (e) { // Process error for request
+  alert(e) // Displays a browser alert with the error message.
+  // This will be the string thrown in line 7 IF the
+  // response code is the reason for jumping to this
+  // catch() function.
+  })
+}
--- app.js
+++ app.js
@@ -24,6 +24,7 @@ app.set('view engine', 'ejs')
 app.use(express.static(path.join(__dirname, './SRC/Public')))
 
 app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
 app.use('/', route)
 app.use('/scheduleAppointment', route)
