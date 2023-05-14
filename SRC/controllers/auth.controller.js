f5d2b899617263a3667d48cd0bb1ca7cc8304b96
SandileW <kunenesandilew@gmail.com>
2023-05-14 15:26:33 +0200

    hash password

--- SRC/controllers/auth.controller.js
+++ SRC/controllers/auth.controller.js
@@ -2,3 +2,34 @@ const User = require('../models/user.schema');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const {builtinModules} = require('module');
+
+const register = (req, res, next) => {
+    bcrypt.hash(req.body.password, 10, function (err, hashedPass) {
+        if (err) {
+            res.json({
+                error: err
+            })
+        } let user = new User({
+            username: req.body.username,
+            email: req.body.email,
+            password: hashedPass
+
+        })
+        user.save()
+            .then(user => {
+                res.json({
+                    message: 'User Added Successfully'
+                })
+                console.log('New user added')
+                
+            })
+            .catch(error => {
+                res.json({
+                    message: 'An error occured'
+                })
+                console.log(error)
+            })
+    })
+
+
+}
