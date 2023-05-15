29187f32539bdb5bc7fe0b6e27a9175a7b955e26
Karabo Kgatla <kkgatla09@gmail.com>
2023-05-15 23:55:28 +0200

    save data to database

--- SRC/models/appointmentSchema.js
+++ SRC/models/appointmentSchema.js
@@ -4,9 +4,14 @@ const Schema = mongoose.Schema
 
 const appointmentSchema = new Schema({
   eventTitle: {
-    type: String
+    type: String,
+    required: true
+  },
+  lecturerName: {
+    type: String,
+    required: true
   }
-})
+}, {timestamps: true})
 
 const Appointment = mongoose.model('Appointment', appointmentSchema)
 
