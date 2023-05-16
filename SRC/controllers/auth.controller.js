7cc7f2476b39f25704d35051dce58f74fbb2fd4c
SandileW <kunenesandilew@gmail.com>
2023-05-16 22:32:32 +0200

    createTime slot function

--- SRC/controllers/auth.controller.js
+++ SRC/controllers/auth.controller.js
@@ -113,6 +113,40 @@ const createAppointment = (req, res, next) => {
         })
     }
 
+    const createTimeslot = (req, res, next) => {
+   
+        const userId = req.session.userId ; //get user id from session
+         
+        User.findOne({ $or: [{ userId: userId }] })
+            .then(user => {
+                if (user) {
+    
+                    let timeslot = new Timeslot({
+    
+                        availabilityTime: req.body.availabilityTime,
+                        numberOfStudents: req.body.numberOfStudents,
+                        date: req.body.date,
+                        userId: userId
+                        
+                    })
+                    timeslot.save()
+                        .then(timeslot => { //associated logged in user with the appointment they schedule
+                            
+                            return User.findByIdAndUpdate(userId, { $push: { timeslots: timeslot} }, { new: true });
+                           
+                        }).then(user => {
+                            res.redirect('/lecturerDashboard');
+                            console.log('New timeslot added');
+    
+                        })
+                        .catch(error => {
+    
+                            console.log(error)
+                        })
+                }
+    
+            })
+        }
 
 
 
