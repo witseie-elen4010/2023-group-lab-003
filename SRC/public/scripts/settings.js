6717d41a7ea9fd6a887766159e67e33b31da5aee
Entropy87 <thamsanqamashinini01@gmail.com>
2023-05-21 00:54:21 +0200

    Checking if update email is valid

--- SRC/public/scripts/settings.js
+++ SRC/public/scripts/settings.js
@@ -19,3 +19,24 @@ async function loadContent (filePath) {
   const content = await response.text()
   document.getElementById('contentContainer').innerHTML = content
 }
+
+//Trying to import this function from signup does not work, need to investigate
+ function validateEmail(emailField, emailError) {
+            if (!emailField.value.match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)) {
+              emailError.innerHTML = 'Please enter a valid email';
+              return false;
+            }
+            emailError.innerHTML = '';
+            return true;
+        }
+        
+        function validateEmailWrapper() {
+            const emailField = document.getElementById('email-field');
+            const emailError = document.getElementById('email-error');
+            validateEmail(emailField, emailError);
+        }
+
+
+
+
+//
