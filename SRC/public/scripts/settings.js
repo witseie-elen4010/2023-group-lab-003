20254fdf285101ce990a39d65cae54d13f70b50a
Entropy87 <thamsanqamashinini01@gmail.com>
2023-05-21 13:42:19 +0200

    Fixing Passwords to allow backend functionality

--- SRC/public/scripts/settings.js
+++ SRC/public/scripts/settings.js
@@ -4,4 +4,15 @@
     document.getElementById('accountButton').addEventListener('click', function(e) {
       e.preventDefault();
       document.getElementById('emailForm').style.display = 'block';
+      document.getElementById('paswordVerifide').style.display = 'none';
+      document.getElementById('paswordNew').style.display = 'none';
+      document.getElementById('paswordConf').style.display = 'none';
+    });
+
+    document.getElementById('passwordButton').addEventListener('click', function(e) {
+      e.preventDefault();
+      document.getElementById('emailForm').style.display = 'none';
+      document.getElementById('paswordVerifide').style.display = 'block';
+      document.getElementById('paswordNew').style.display = 'block';
+      document.getElementById('paswordConf').style.display = 'block';
     });
