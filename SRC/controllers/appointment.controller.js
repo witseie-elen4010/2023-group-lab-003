931e0f6a73ee7063c98fbd72ed01c6bd1b14efe0
Karabo Kgatla <kkgatla09@gmail.com>
2023-05-21 19:41:12 +0200

    update the appointment controller

--- SRC/controllers/appointment.controller.js
+++ SRC/controllers/appointment.controller.js
@@ -20,16 +20,28 @@ const createAppointment = (req, res, next) => {
         .then(appointment => { //associated logged in user with the appointment they schedule
           return User.findByIdAndUpdate(userId, { $push: { appointments: appointment } }, { new: true });
         })
-        .then(user => {
+        .then(student =>{
+          return User.findOne({ name: req.body.lecturerName, role: 'lecture' });
+        })
+        .then(lecturer => {
+          if (!lecturer) {
+            // Handle case when the specified lecturer is not found
+            throw new Error('Lecturer not found');
+          }
+          // Associate the appointment with the lecturer
+          lecturer.appointments.push(savedAppointment);
+          return lecturer.save();
+        })
+        .then(() => {
           res.redirect('/studentDashboard');
-          console.log('New appointment added');
         })
         .catch(error => {
           console.log(error)
-        })
+          res.status(500).json({ message: 'Failed to create appointment' });
+        });
       }
-    })
-}
+  });
+};
 
 module.exports = {
   createAppointment
