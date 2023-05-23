253a70894a13a5bea4c25485b731de1f5bade3f2
Entropy87 <thamsanqamashinini01@gmail.com>
2023-05-23 16:30:40 +0200

    Creating search function

--- SRC/routes/main.routes.js
+++ SRC/routes/main.routes.js
@@ -78,6 +78,36 @@ router.get('/searchAppointment', (req, res) => {
   res.render('searchAppointment');
 })
 
+router.post('/searchAppointments', async (req, res, next) => {
+  const userId = req.session.userId;
+  const searchQuery = req.body.search;
+
+  try {
+    const user = await User.findOne({ _id: userId });
+
+    if (!user) {
+      return res.status(404).render('error', { message: 'User not found' });
+    }
+
+    let condition = {};
+    if (searchQuery) {
+      condition = {
+        $or: [
+          { eventTitle: { $regex: new RegExp(searchQuery, "i") } },
+          { lecturerName: { $regex: new RegExp(searchQuery, "i") } }
+        ]
+      };
+    }
+
+    const appointments = await Appointment.find(condition);
+
+    res.status(200).json({ appointments });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
 
 // ------------------- Schedule Appointment -----------------------
 // showing schedule appointment form
