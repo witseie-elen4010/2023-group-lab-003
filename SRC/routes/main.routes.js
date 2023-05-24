62f45fe6212f2a8f24cda51156d1e2fa12e5ccef
Entropy87 <thamsanqamashinini01@gmail.com>
2023-05-24 10:55:25 +0200

    Join button adds appointment

--- SRC/routes/main.routes.js
+++ SRC/routes/main.routes.js
@@ -231,10 +231,31 @@ router.get('/cancel/:id', (req, res) => {
 });
 
 //Join Appointment
-router.get('/Join',(req, res) => {
+router.get('/Join', async (req, res) => {
   const appointmentId = req.query.appointmentId;
-  console.log(appointmentId);
+  const userId = req.session.userId;
+
+  try {
+    const appointment = await Appointment.findById(appointmentId);
+    if (!appointment) {
+      return res.status(404).send({ message: 'Appointment not found' });
+    }
+
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).send({ message: 'User not found' });
+    }
+
+    await User.findByIdAndUpdate(userId, { $push: { appointments: appointmentId } });
+
+    res.send({ message: 'Joined the appointment successfully' });
+  } catch (error) {
+    res.status(500).send({ message: 'Server error' });
+  }
+
+  
 });
+
 
 
 module.exports = router
