d3a0c581413936cf6035395bdcc79ecf99ea8928
Entropy87 <thamsanqamashinini01@gmail.com>
2023-05-27 21:43:34 +0200

    Refactoring timeslots schema

--- SRC/models/timeslots.schema.js
+++ SRC/models/timeslots.schema.js
@@ -3,23 +3,51 @@ const mongoose = require('mongoose');
 
 
 const timeslotSchema = new mongoose.Schema ({
-    availabilityTime: {
 
-        type: String,
-        
-    },
-    numberOfStudents: {
-        type: String,
-        
-    },
-    date:{
-        type: String,
-        
-    },
-    user: { //link the person who set an appointment
-        type: mongoose.Types.ObjectId,
-        ref: 'users'
-      }
+  Monday:{
+    Time: { type: String },
+    numberOfStudents: {type:Number},
+    MaxNumberOfConsulation: {type:Number}
+  },
+
+  Tuesday:{
+    Time: { type: String },
+    numberOfStudents: {type:Number},
+    MaxNumberOfConsulation: {type:Number}
+  },
+
+  Wednesday:{
+    Time: { type: String },
+    numberOfStudents: {type:Number},
+    MaxNumberOfConsulation: {type:Number}
+  },
+
+  Thursday:{
+    Time: { type: String },
+    numberOfStudents: {type:Number},
+    MaxNumberOfConsulation: {type:Number}
+  },
+
+  Friday:{
+    Time: { type: String },
+    numberOfStudents: {type:Number},
+    MaxNumberOfConsulation: {type:Number}
+  },
+
+
+   // availabilityTime: {
+   //     type: String,  
+  //  },
+  //  numberOfStudents: {
+  //      type: String,  
+  //  },
+  //  date:{
+  //      type: String,    
+  //  },
+  //  user: { //link the person who set an appointment
+  //      type: mongoose.Types.ObjectId,
+  //      ref: 'users'
+  //    }
   
 }, {timestamps: true});
 
