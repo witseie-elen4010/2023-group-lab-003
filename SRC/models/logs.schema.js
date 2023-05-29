8dfb0bf407b615f3e590ee816026260f23201329
SandileW <kunenesandilew@gmail.com>
2023-05-29 12:34:15 +0200

    logs schema

---
+++ SRC/models/logs.schema.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const loggingSchema = new mongoose.Schema({
+    userId:{
+        type: mongoose.Types.ObjectId,
+        ref: 'users',
+        required: true,
+    },
+    action:{
+        type: String,
+        required: true,
+    },
+    timestamp: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+const Log = mongoose.model('Log', loggingSchema);
